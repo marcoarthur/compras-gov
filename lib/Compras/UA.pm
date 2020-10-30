9fb1c2345a76d37fcb86d8bbf9ee9fd16d4ab292
Marco Arthur <arthurpbs@gmail.com>
2020-10-30 16:45:47 -0300

    Initial

---
+++ lib/Compras/UA.pm
@@ -0,0 +1,39 @@
+package Compras::UA;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Compras::UA - It's new $module
+
+=head1 SYNOPSIS
+
+    use Compras::UA;
+
+=head1 DESCRIPTION
+
+Compras::UA is ...
+
+=head1 LICENSE
+
+Copyright (C) Marco Arthur.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Marco Arthur E<lt>arthurpbs@gmail.comE<gt>
+
+=cut
+
---
+++ t/00_compile.t
@@ -0,0 +1,9 @@
+use strict;
+use Test::More 0.98;
+
+use_ok $_ for qw(
+    Compras::UA
+);
+
+done_testing;
+
