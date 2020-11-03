1d3a6cc9be7258b34b8a1b822f7f897915efe2af
Marco Arthur <arthurpbs@gmail.com>
2020-11-03 11:45:40 -0300

    New model Institution
    Add Institution (Orgaos) as data model

---
+++ lib/Compras/Model/Institutions.pm
@@ -0,0 +1,16 @@
+package Compras::Model::Institutions;
+use Mojo::Base 'Compras::Model';
+
+has attributes => sub {
+    {
+        ativo              => 'Se o órgão está ativo.',
+        codigo             => 'Código do órgão',
+        codigo_siorg       => 'Código do siorg do órgão',
+        codigo_tipo_adm    => 'Código do tipo da administração do órgão',
+        codigo_tipo_esfera => 'Tipo da esfera do órgão',
+        codigo_tipo_poder  => 'Código do tipo do poder do órgão',
+        nome               => 'Nome do órgão.',
+    };
+};
+
+1;
