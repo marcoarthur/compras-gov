461e51f9425b54596e516d1de99a78bac1e91cc9
Marco Arthur <arthurpbs@gmail.com>
2020-11-03 06:45:25 -0300

    Parse the return super structure

--- lib/Compras/RSet.pm
+++ lib/Compras/RSet.pm
@@ -1,8 +1,31 @@
 package Compras::RSet;
 use Mojo::Base -base, -signatures;
 use Mojo::Exception qw(raise);
+use Mojo::JSON::Pointer;
 
 has tx => sub { die "Required attrib tx" };
+has json_structure => sub {
+	{
+		results => '/_embedded',
+		links => '/_links',
+		count => '/count',
+		offset => '/offset',
+	}
+};
+
+# validate json response structure
+sub _validate_json( $self, $json_obj )  {
+    my $pointer = Mojo::JSON::Pointer->new($json_obj);
+    my $parsed = {};
+
+    while( my ($key, $member) = each %{ $self->json_structure } ) {
+	if ( ! $pointer->contains($member) )  {
+	    raise 'Compras::Exception', "Invalid Server Response missing $member";
+	}
+	$parsed->{ $key } = $pointer->get($member);
+    }
+    return $parsed;
+}
 
 # validate http request result
 sub _validate( $self ) {
@@ -29,7 +52,8 @@ sub parse( $self ) {
 
 # Parse a JSON object
 sub _parse_model_json( $self, $res_obj ) {
-    return $res_obj;
+    my $structure = $self->_validate_json($res_obj);
+    return $structure;
 }
 
 # Parse Html or CSS or other type
