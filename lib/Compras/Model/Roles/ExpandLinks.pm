4b404792aaae5a3e77a735b41c15e1fd85db7c82
Marco Arthur <arthurpbs@gmail.com>
2020-11-22 18:32:46 -0300

    Refactor
    Major refactor on role to be more flexible and code more readable

--- lib/Compras/Model/Roles/ExpandLinks.pm
+++ lib/Compras/Model/Roles/ExpandLinks.pm
@@ -7,30 +7,49 @@ use Mojo::UserAgent;
 has base_url => sub { Mojo::URL->new('http://compras.dados.gov.br') };
 has _ua      => sub { Mojo::UserAgent->new };
 has log      => sub { die "Requires a logger" };
+has links    => sub {
+    my $self = shift;
+    my $links = $self->_other->{_links};
+    return Mojo::Collection->new( @$links{ $self->_topics } )->each(
+        sub ( $e, $n ) {
+            my $url = Mojo::URL->new( $e->{href} )->base( $self->base_url );
+            $url->path->parts->[-1] .= '.json';    # append format
+            $e->{href} = $url->to_abs;             # update href
+        }
+    );
+};
 
-sub expand_links( $self ) {
+has accessor => sub { 'expanded_data' };
+
+# Get from _links hash references containing an href key
+sub _topics( $self ) {
     my $links = $self->_other->{_links};
 
-    for my $l ( keys %$links ) {
-        next if $l eq "self" or $l eq "first";    # auto link, ignores
-        my $link = $links->{$l};
-        next unless ref $link eq 'HASH';          # no-expandable field
-        next unless $link->{href};                # no link to follow
-        my $path = $link->{href};
-        my $url  = Mojo::URL->new($path)->base( $self->base_url );
-        $url->path->parts->[-1] .= '.json';       # append format
+    my @topics = grep {
+        $_ !~ /self|first/ &&                      # not an autolink
+          ref $links->{$_} eq 'HASH' &&            # expandable
+          exists $links->{$_}->{href}              # has link to follow
+    } keys %$links;
 
-        # blocking fetch
-        $self->log->info("Following " . $url->to_abs);
-        my $res = $self->_ua->get( $url->to_abs )->result;
+    return @topics;
+}
 
-        if ( $res->is_error ) {
-            raise 'Compras::Exception', "Error fetching $url";
-        } else {
-            $self->log->info("Saving into $l");
-            $links->{$l}->{expanded_data} = $res->json;
+sub expand_links( $self ) {
+    $self->log->info( "Expanding info for " . join " ", $self->_topics );
+    $self->links->each(
+        sub ( $e, $n ) {
+            my $url = $e->{href};
+            $self->log->info( "Following " . $url );
+            my $res = $self->_ua->get( $url )->result;
+
+            if ( $res->is_error ) {
+                raise 'Compras::Exception', "Error fetching $url";
+            } else {
+                $self->log->info("Saving info");
+                $e->{$self->accessor} = $res->json;
+            }
         }
-    }
+    );
 }
 
 1;
