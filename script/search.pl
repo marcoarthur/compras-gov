c157bfd37513d6c300a8a24a084b97e77ee2426a
Marco Arthur <arthurpbs@gmail.com>
2020-12-01 11:51:59 -0300

    Refactor
    Make script.pl more readable separate data from code

--- script/search.pl
+++ script/search.pl
@@ -28,103 +28,19 @@ my $default_cb = sub ( $data ) {
     return $data;
 };
 
-my @searches = (
-
-    # all providers from Ubatuba city ( id 72095 )
-    {
-        description => 'all providers from Ubatuba city ( id 72095 )',
-        search      => {
-            module => 'fornecedores',
-            params => { id_municipio => 72095 }
-        },
-        cb  => $default_cb,
-        run => 0,
-    },
-
-    # bids from provider (id 538083)
-    {
-        description => 'bids from provider (id 538083)',
-        search      => {
-            module => 'licitacoes',
-            params => { id_fornecedor => 538083 }
-        },
-        cb  => $default_cb,
-        run => 0,
-
-    },
-
-    # find bids from ubatuba providers
-    {
-        description => 'bids from Ubatuba-SP providers',
-        search      => {
-            module => 'fornecedores',
-            params => { id_municipio => 72095 }
-        },
-        cb => $default_cb,
-        run => 0,
-    },
-
-    # find gov contracts involving personal contractors
-    {
-        description => 'contracts involving personal contractors',
-        search      => {
-            module => 'contratos',
-            params => { tipo_pessoa => 'PF' }
-        },
-        cb  => $default_cb,
-        run => 0,
-
-    },
-
-    # find gov contracts involving no licitation of contractors during 2020 up to 50.000
-    {
-        description => 'no licitation contracts during 2020 and value up to 50.000 BRL',
-        search      => {
-            module => 'contratos',
-            params => {
-                modalidade               => 6,
-                data_inicio_vigencia_min => '2020-01-01',
-                valor_inicial_min        => 50000
-            }
-        },
-        cb  => $default_cb,
-        run => 0,
-
-    },
-
-    # find gov contracts during 2020 up to 100.000
-    {
-        description => 'contracts during 2020,  value up to 100.000 BRL',
-        search      => {
-            module => 'contratos',
-            params => {
-                data_inicio_vigencia_min => '2020-01-01',
-                valor_inicial_min        => 2 * 50000
-            }
-        },
-        cb  => $default_cb,
-        run => 0,
-
-    },
-
-    # find uasg descriptions
-    {
-        description => 'find uasg description',
-        search      => {
-            module  => 'licitacoes',
-            method  => 'uasg',
-            params  => { id => 200015 },
-            req_def => 1,
-        },
-        cb => sub ($data) { p $data; return $data },
-    }
+my %cbs = (
+    default_cb => $default_cb
 );
+
+my $data = require './script/data/searches.conf';
+my @searches = @$data;
 
 sub do_search {
     my %values = @_;
     $log->info("Searching $values{description} ...");
-    my $s = Compras::Search->new( query => $values{search}, tout => 380 );
-    return $values{cb}->( $s->search );
+    my $s = Compras::Search->new( query => $values{search} );
+    return $values{cb}->( $s->search ) if ref $values{cb};
+    return $cbs{ $values{cb} }->( $s->search );
 }
 
 sub run_all {
@@ -136,7 +52,7 @@ sub run_all {
             $log->info(
                 "Can't find search data (index: $index) select 0 up to $#searches, options are");
             for my $i ( 0 .. $#searches ) {
-                $log->info("$i : $searches[$i]->{description}");
+                $log->info("$i : $searches[$i]{description}");
             }
             exit 1;
         }
